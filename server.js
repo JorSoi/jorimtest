56f7974131d01a12ab7550052198b1a5829590e1
Jorim Soika <jorim.soika@gmx.de>
2022-10-24 20:24:43 +0800

    test

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const Pool = require('node-postgres').Pool;
+const Pool = require('pg').Pool;
 const pool = new Pool({
-    
+  host: "ec2-54-163-34-107.compute-1.amazonaws.com",
+  database: "d1ffgq0tk1jv51",
+  user: "yvudtjxqpguwua",
+  port: 5432,
+  password: "698c669ee5c7394a8d0b6d1889027057e5eb9a205bec2cbf5bd5d53ccba332e9"
 })
 
 
