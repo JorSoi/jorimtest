d8364e0bef92ba2bee300414190373d4b873cdd9
Jorim Soika <jorim.soika@gmx.de>
2022-10-24 20:28:56 +0800

    d

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   host: "ec2-54-163-34-107.compute-1.amazonaws.com",
   database: "d1ffgq0tk1jv51",
   user: "yvudtjxqpguwua",
-  port: 5432,
+  port: "5432",
   password: "698c669ee5c7394a8d0b6d1889027057e5eb9a205bec2cbf5bd5d53ccba332e9"
 })
 
